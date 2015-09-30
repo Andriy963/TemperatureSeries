27979fc51a1ce845d3efdd966b4230567d80c6e1
Andriy963 <Adriano963@gmail.com>
2015-09-30 19:04:51 +0300

    Update TempSummaryStatistics.java

--- src/main/java/ua/yandex/shad/tempseries/TempSummaryStatistics.java
+++ src/main/java/ua/yandex/shad/tempseries/TempSummaryStatistics.java
@@ -1,48 +1,48 @@
 package ua.yandex.shad.tempseries;
 
 public class TempSummaryStatistics {
-	public static final double epsilon = 0.0000001;
-	private final double avgTemp;
-	private final double devTemp;
-	private final double minTemp;
-	private final double maxTemp;
-	
-	public TempSummaryStatistics() {
-		this.avgTemp = 0;
-		this.devTemp = 0;
-		this.minTemp = 0;
-		this.maxTemp = 0;
-	}
-	
-	public TempSummaryStatistics(double avg, double dev, 
-	double min, double max) {
-		this.avgTemp = avg;
-		this.devTemp = dev;
-		this.minTemp = min;
-		this.maxTemp = max;
-	}
-	
-	@Override
-	public boolean equals(Object equal) {
-		
-		TempSummaryStatistics eq = (TempSummaryStatistics) equal;
-		if (Math.abs(this.avgTemp - eq.avgTemp) > epsilon) {
-			return false;
-		}
-		if (Math.abs(this.devTemp - eq.devTemp) > epsilon) {
-			return false;
-		}
-		if (Math.abs(this.minTemp - eq.minTemp) > epsilon) {
-			return false;
-		}
-		if (Math.abs(this.maxTemp - eq.maxTemp) > epsilon) {
-			return false;
-		}
-		return true;
-	}
-	
-	public int hashCode() {
-		assert false : "hashCode not designed";
-		return 42;
+     public static final double EPSILON = 0.0000001;
+     private final double avgTemp;
+     private final double devTemp;
+     private final double minTemp;
+     private final double maxTemp;
+     
+     public TempSummaryStatistics() {
+          this.avgTemp = 0;
+          this.devTemp = 0;
+          this.minTemp = 0;
+          this.maxTemp = 0;
+     }
+     
+     public TempSummaryStatistics(double avg, double dev, 
+     double min, double max) {
+          this.avgTemp = avg;
+          this.devTemp = dev;
+          this.minTemp = min;
+          this.maxTemp = max;
+     }
+     
+     @Override
+     public boolean equals(Object equal) {
+          
+          TempSummaryStatistics eq = (TempSummaryStatistics) equal;
+          if (Math.abs(this.avgTemp - eq.avgTemp) > EPSILON) {
+               return false;
+          }
+          if (Math.abs(this.devTemp - eq.devTemp) > EPSILON) {
+               return false;
+          }
+          if (Math.abs(this.minTemp - eq.minTemp) > EPSILON) {
+               return false;
+          }
+          if (Math.abs(this.maxTemp - eq.maxTemp) > EPSILON) {
+               return false;
+          }
+          return true;
+     }
+     
+     public int hashCode() {
+          assert false : "hashCode not designed";
+          return 42;
   }
 }
